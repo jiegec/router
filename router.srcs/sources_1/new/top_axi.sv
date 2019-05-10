09b1bcac4cfada119074bc0db58f1b34b3e58003
Jiajie Chen <noc@jiegec.ac.cn>
2019-05-10 08:00:07 +0800

    Fix multi driven net warning

--- router.srcs/sources_1/new/top_axi.sv
+++ router.srcs/sources_1/new/top_axi.sv
@@ -314,18 +314,6 @@ module top_axi(
             fifo_matrix_wready[1][`OS_PORT_ID] <= 0;
             fifo_matrix_wready[2][`OS_PORT_ID] <= 0;
             fifo_matrix_wready[3][`OS_PORT_ID] <= 0;
-            fifo_matrix_wdata[`OS_PORT_ID][0] <= 0;
-            fifo_matrix_wdata[`OS_PORT_ID][1] <= 0;
-            fifo_matrix_wdata[`OS_PORT_ID][2] <= 0;
-            fifo_matrix_wdata[`OS_PORT_ID][3] <= 0;
-            fifo_matrix_wlast[`OS_PORT_ID][0] <= 0;
-            fifo_matrix_wlast[`OS_PORT_ID][1] <= 0;
-            fifo_matrix_wlast[`OS_PORT_ID][2] <= 0;
-            fifo_matrix_wlast[`OS_PORT_ID][3] <= 0;
-            fifo_matrix_wvalid[`OS_PORT_ID][0] <= 0;
-            fifo_matrix_wvalid[`OS_PORT_ID][1] <= 0;
-            fifo_matrix_wvalid[`OS_PORT_ID][2] <= 0;
-            fifo_matrix_wvalid[`OS_PORT_ID][3] <= 0;
         end else begin
             if (!fifo_matrix_rx_progress && os_rxd_tready) begin
                 // can send to os now
@@ -411,6 +399,19 @@ module top_axi(
             fifo_matrix_tx_wea <= 0;
 
             os_txd_tready <= 1;
+
+            fifo_matrix_wdata[`OS_PORT_ID][0] <= 0;
+            fifo_matrix_wdata[`OS_PORT_ID][1] <= 0;
+            fifo_matrix_wdata[`OS_PORT_ID][2] <= 0;
+            fifo_matrix_wdata[`OS_PORT_ID][3] <= 0;
+            fifo_matrix_wlast[`OS_PORT_ID][0] <= 0;
+            fifo_matrix_wlast[`OS_PORT_ID][1] <= 0;
+            fifo_matrix_wlast[`OS_PORT_ID][2] <= 0;
+            fifo_matrix_wlast[`OS_PORT_ID][3] <= 0;
+            fifo_matrix_wvalid[`OS_PORT_ID][0] <= 0;
+            fifo_matrix_wvalid[`OS_PORT_ID][1] <= 0;
+            fifo_matrix_wvalid[`OS_PORT_ID][2] <= 0;
+            fifo_matrix_wvalid[`OS_PORT_ID][3] <= 0;
         end else begin
             // save data whenever tvalid is 1
             // and begin to send when tlast is 1
